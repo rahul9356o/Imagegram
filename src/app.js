ae03fd638dca0ffa877f5fdf26042778ea404ed6
Rahul Vijay Nalawade <rvnalawade8@gmail.com>
2024-12-26 23:12:02 +0530

    respons commit

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ import userRouter from "./routes/users.routes.js";
 
 app.use("api/v1/users",userRouter);
 
-// http://localhost:2000/api/v1/users/register
+// http://localhost:8000/api/v1/users/register
 
 
 export default app;
